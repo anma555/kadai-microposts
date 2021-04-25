f867d6d527076893c5dd31a53b5639c5a81f0085
shigeru.yamabayashi <anma555@me.com>
2021-04-25 06:00:04 +0000

    favorite function modification

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,13 @@ class FavoritesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.add_favorite(micropost)
     flash[:success] = 'お気に入りに追加しました。'
-    redirect_to current_user
+    redirect_back(fallback_location: root_url)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unfavorite(micropost)
     flash[:success] = 'お気に入りから削除しました。'
-    redirect_to current_user
+    redirect_back(fallback_location: root_url)
   end
 end
